5ae3e8a7e6271b2d71e71966fe9b5f2820cc27b7
前田 <sloth19920731@gmail.com>
2021-10-22 00:20:09 +0900

    Dynamodbではなく、APIGatewayと通信するために土台だけ残す

--- lambda_function.py
+++ lambda_function.py
@@ -12,27 +12,5 @@ def lambda_handler(event,context):
     all=soup.find_all("p",{"class":"jlnpc-kuchikomiCassette__postBody"})
 
     for item in all:
-        # シーケンスデータを得る
-        seqtable = dynamoDB.Table('Sequence')
-        nextseq = get_next_seq(seqtable, 'Comments')
-        table.put_item(
-            Item = {
-                'id' : nextseq,
-                'comment': item.text,
-            }
-        )
-        print('クチコミをDBに登録しました')
+        print(item.text)
 
-# 連番を裁判して返す関数
-def get_next_seq(table, tablename):
-	response = table.update_item(
-		Key = {
-			'tablename' : tablename
-		},
-		UpdateExpression='set seq = seq + :val',
-		ExpressionAttributeValues = {
-			':val' : 1
-		},
-		ReturnValues='UPDATED_NEW'
-	)
-	return response['Attributes']['seq']
